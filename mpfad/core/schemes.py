63aeb34a499a8d4b5c82d052509d018e4ad2363a
Filipe Cumaru <facsa@cin.ufpe.br>
2022-08-03 16:36:59 -0300

    fix: add sink/source terms

--- mpfad/core/schemes.py
+++ mpfad/core/schemes.py
@@ -45,9 +45,10 @@ class MpfadScheme(object):
 
         A_D, q_D = self._handle_dirichlet_bc()
         q_N = self._handle_neumann_bc()
+        q_source = self.mesh.source_term[:].flatten()
 
         A = A_tpfa + A_cdt + A_D
-        q = q_D + q_N - q_cdt
+        q = q_D + q_N - q_cdt + q_source
 
         return A, q
 
