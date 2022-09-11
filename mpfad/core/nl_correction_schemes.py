5422a6801117b96488881ec7eedc00067320538f
Filipe Cumaru <facsa@cin.ufpe.br>
2022-09-11 12:54:42 -0300

    feat: add some helper methods to MPFA-DNL's class
    Add methods to compute the L2 error and LDU decomposition of a matrix.

--- mpfad/core/nl_correction_schemes.py
+++ mpfad/core/nl_correction_schemes.py
@@ -1,4 +1,7 @@
 from mpfad.core.schemes import MpfadScheme
+from scipy.sparse.linalg import splu, spsolve
+from scipy.sparse import diags, csc_matrix
+import numpy as np
 
 
 class BaseNonLinearCorrection(object):
@@ -18,3 +21,17 @@ class MpfadNonLinearDefectionCorrection(BaseNonLinearCorrection):
 
     def run(self, assemble_mpfad_matrix=True):
         super().run(assemble_mpfad_matrix)
+
+    def _compute_ldu_decomposition(self, M):
+        lu_decomp = splu(M)
+        diag_U = lu_decomp.U.diagonal()
+        L = lu_decomp.L
+        D = diags(diag_U)
+        U = lu_decomp.U / diag_U[:, None]
+
+        return L, D, U
+
+    def _compute_l2_error(self, u1, u2):
+        V = self.mesh.volumes.volume[:]
+        l2_err = (np.sum(((u1 - u2) ** 2) * V) / np.sum((u1 ** 2) * V)) ** 0.5
+        return l2_err
