45ae01faaaf4ed75d81680c24ba4ebc8fb78205b
Filipe Cumaru <facsa@cin.ufpe.br>
2022-09-11 12:53:15 -0300

    feat: add MPFA-DNL's boilerplate

--- mpfad/core/nl_correction_schemes.py
+++ mpfad/core/nl_correction_schemes.py
@@ -9,3 +9,12 @@ class BaseNonLinearCorrection(object):
     def run(self, assemble_mpfad_matrix=True):
         if assemble_mpfad_matrix:
             self.mpfad.assemble()
+
+
+class MpfadNonLinearDefectionCorrection(BaseNonLinearCorrection):
+    def __init__(self, mesh, mpfad_scheme: MpfadScheme, dmp_tol=1e-6):
+        super().__init__(mesh, mpfad_scheme)
+        self.dmp_tol = dmp_tol
+
+    def run(self, assemble_mpfad_matrix=True):
+        super().run(assemble_mpfad_matrix)
